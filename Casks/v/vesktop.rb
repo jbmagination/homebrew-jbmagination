91e03d3b4187bb60e0ed7007db9d09e05354a171
JBMagination <26027089+jbmagination@users.noreply.github.com>
2024-10-21 13:17:33 -0400

    Update vesktop.rb

--- Casks/v/vesktop.rb
+++ Casks/v/vesktop.rb
@@ -1,11 +1,7 @@
 cask "vesktop" do
-  version "0.4.4"
-  sha256 on_arch_conditional arm: "4665fa89ca9ab3aaad2d216697d6a513b01247bf3159e1aec73cd9bd9c873644",
-    intel: "64f9b28e4bb6509f6b930cad89be4eb66910fec653291316599b075d52b35860"
-
-  url on_arch_conditional arm: "https://github.com/Vencord/Vesktop/releases/download/v#{version}/Vesktop-#{version}-arm64.dmg",
-    intel: "https://github.com/Vencord/Vesktop/releases/download/v#{version}/Vesktop-#{version}.dmg"
-  
+  version "1.5.3"
+  sha256 "71e39434748e684a826f390ceb846d52ed1886bab8b5385c5d94c377e3ac1172"
+  url "https://github.com/Vencord/Vesktop/releases/download/v#{version}/Vesktop-#{version}.dmg"
   name "Vesktop"
   desc "Discord web wrapper that comes with Vencord pre-installed"
   homepage "https://github.com/Vencord/Vesktop"
